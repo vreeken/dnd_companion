34c9cacca99b2eff79525a931459518768a3ecde
Michael Vreeken <mike@vreeken.dev>
2019-04-03 20:13:38 -0700

    Add jwt to axios by default

--- public/js/posts.js
+++ public/js/posts.js
@@ -6817,6 +6817,7 @@ Vue.config.productionTip = false;
 
 window.axios = __webpack_require__(/*! axios */ "./node_modules/axios/index.js");
 window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
+window.axios.defaults.headers.common['Authorization'] = 'Bearer ' + localStorage.getItem('jwt');
 /**
  * Next we will register the CSRF Token as a common header with Axios so that
  * all outgoing HTTP requests automatically have it attached. This is just
