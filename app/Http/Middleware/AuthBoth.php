b5fdbc1f299893e907705a07122529a9452e99f7
Michael Vreeken <mike@vreeken.dev>
2019-04-05 21:07:48 -0700

    Probably won't be used

---
+++ app/Http/Middleware/AuthBoth.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Auth;
+use JWTAuth;
+use Tymon\JWTAuth\Exceptions\JWTException;
+
+use Closure;
+
+use Log;
+
+class AuthBoth {
+	/**
+	 * Handle an incoming request.
+	 *
+	 * @param  \Illuminate\Http\Request  $request
+	 * @param  \Closure  $next
+	 * @return mixed
+	 */
+	public function handle($request, Closure $next) {
+		$user=null;
+		
+		try {
+			$user = JWTAuth::toUser(JWTAuth::getToken());
+		} catch (JWTException $e) {
+			$user = Auth::user();
+		}
+
+		if ($user === null) {
+			if ($request->expectsJson()) {
+				return response()->json(['error'=>'unauthenticated'], 401);
+			}
+			//TODO test this:
+			return redirect('auth/login');
+		}
+
+		return $next($request);
+	}
+}
