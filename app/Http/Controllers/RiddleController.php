7629cc30b880a41c56f0eb5c915285620c9fbab9
Michael Vreeken <mike@vreeken.dev>
2019-08-04 16:20:57 -0700

    Fix Riddle submissions

--- app/Http/Controllers/RiddleController.php
+++ app/Http/Controllers/RiddleController.php
@@ -14,15 +14,18 @@ class RiddleController extends ParentPostController {
 	}
 
     public function submitPost(Request $request) {
-		if (!$request->has('riddle') || !$request->has('answer')) {
+		if (!$request->has('riddle') && !$request->has('riddle_body')) {
+			return response()->json(['error'=>'invalid_parameters'], 400);
+		}
+		if (!$request->has('answer') && !$request->has('riddle_answer')) {
 			return response()->json(['error'=>'invalid_parameters'], 400);
 		}
 
 		$post = new Riddle;
 		$postVote = new RiddleVote;
 
-		$post->riddle = $request->input('riddle');
-        $post->answer = $request->input('answer');
+		$post->riddle = $request->has('riddle') ? $request->input('riddle') : $request->has('riddle_body');
+        $post->answer = $request->has('answer') ? $request->input('answer') : $request->input('riddle_answer');
 
 		return $this->_submitPost($post, $postVote, $request);
 	}
